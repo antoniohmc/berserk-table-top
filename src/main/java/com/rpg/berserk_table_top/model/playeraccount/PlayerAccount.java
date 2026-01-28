dad791ebb9ef9653742e5bf74a5ea3263e6758a9
Antonio Campos <antoniomallmann01@gmail.com>
2026-01-28 12:18:48 -0300

    nickname

--- src/main/java/com/rpg/berserk_table_top/model/playeraccount/PlayerAccount.java
+++ src/main/java/com/rpg/berserk_table_top/model/playeraccount/PlayerAccount.java
@@ -18,7 +18,7 @@ public class PlayerAccount {
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String nickName;
+    private String nickname;
     private String email;
     private Integer password;
 
