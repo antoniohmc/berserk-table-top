08ea1596ff0fe04e873685d3a5de958c614304e5
Antonio Campos <antoniomallmann01@gmail.com>
2026-01-28 12:12:55 -0300

    Alterando nome do atributo "nome" para "nickName"

--- src/main/java/com/rpg/berserk_table_top/model/playeraccount/PlayerAccount.java
+++ src/main/java/com/rpg/berserk_table_top/model/playeraccount/PlayerAccount.java
@@ -18,7 +18,7 @@ public class PlayerAccount {
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
+    private String nickName;
     private String email;
     private Integer password;
 
